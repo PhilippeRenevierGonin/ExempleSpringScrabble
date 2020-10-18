833490151e165b6778a47f163eaee6bb7d6c4d05
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2020-10-18 17:13:26 +0200

    Utilisation d'un thread pour "sortir" l'exécution de la partie de la connexioon. Attention à la synchro...

--- serveur/src/main/java/prg/exemple/demoscrabble/Moteur.java
+++ serveur/src/main/java/prg/exemple/demoscrabble/Moteur.java
@@ -6,12 +6,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Scope("singleton")
-public class Moteur {
+public class Moteur implements Runnable {
     @Autowired
     MoteurWebControlleur ctrl;
 
+    Thread partie ;
+
     public void lancerPartie() {
+        if (partie == null) {
+            System.out.println("Moteur > la partie est démarrée");
+            partie = new Thread(this);
+            partie.start();
+        } else {
+            System.out.println("Moteur > la partie est déjà démarrée");
+        }
+    }
+
+    @Override
+    public void run() {
         String motJoué = ctrl.demanderAuJoueurDeJoueur() ;
         System.out.println("Moteur > le joueur a joué : "+motJoué);
+        System.out.println("Moteur > la partie est finie");
+        partie = null;
     }
 }
